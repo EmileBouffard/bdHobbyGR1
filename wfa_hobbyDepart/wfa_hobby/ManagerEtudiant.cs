5067255b7307f8f69fcf76fb1709e124fd61c108
EmileBouffard <157501330+EmileBouffard@users.noreply.github.com>
2024-02-16 09:44:10 -0500

    verification

---
+++ wfa_hobbyDepart/wfa_hobby/ManagerEtudiant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_hobby
+{
+    internal class ManagerEtudiant
+    {
+        public int AjouterEtudiant(Etudiant etudiant)
+        {
+            int nombreDeLigneAffecte = 0;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
