9ad580a5ad86cf17c6fb8332b969ca08239f59f0
EmileBouffard <157501330+EmileBouffard@users.noreply.github.com>
2024-02-16 09:29:57 -0500

    create etudiant

--- wfa_hobbyDepart/wfa_hobby/ajouterEtudiantForm.cs
+++ wfa_hobbyDepart/wfa_hobby/ajouterEtudiantForm.cs
@@ -16,14 +16,46 @@ namespace wfa_hobby
         {
             InitializeComponent();
         }
-       
+
         private void ajouterEtudiantForm_Load(object sender, EventArgs e)
         {
-           
-        }
 
-        private void ajouterButton_Click(object sender, EventArgs e)
+        }
+        private Boolean textBoxSontRemplis()
         {
+            return (prenomTextBox.Text != "" && nomTextBox.Text != "" && cellulaireTextBox.Text != "");
+        }
+        private Etudiant PrendreLesValeursDesTextBox()
+        {
+            Etudiant etudiant = new Etudiant();
+            etudiant.Prenom = prenomTextBox.Text;
+            etudiant.Nom = nomTextBox.Text;
+            etudiant.Cellulaire = cellulaireTextBox.Text;
+            etudiant.Humour = (int)humourNumericUpDown.Value;
+            etudiant.No_provenance = 1; //remplacer plus tard
+            return etudiant;
+        }
+        
+        
+        private void ajouterButton_Click(object sender, EventArgs e)
+        {   Etudiant etudiant = new Etudiant();
+            try 
+            {
+                if (textBoxSontRemplis())
+                {
+                    //prendre les valeur
+                    etudiant = PrendreLesValeursDesTextBox();
+                    //appeler fonction ajout
+                }
+                else
+                {
+                MessageBox.Show(" Entrez toutes le données ");
+                }
+            } 
+            catch(Exception)
+            {
+                throw;
+            } 
         }
     }
 }
